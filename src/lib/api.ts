08a715288cdbe32d940348ef5be2a0e967e3e290
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-10-26 12:27:57 +0900

    Tweak configuration to use Page Function

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 export async function reviewAnswer(message: string) {
-  const response = await axios.post<string>("/api", message, {
+  const response = await axios.post("/api", message, {
     headers: {
       "Content-Type": "text/plain",
     },
   });
-  return response;
+  const reviewResult = response.data;
+  return reviewResult;
 }
