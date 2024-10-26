e1da4f59a0a95e491b77ff9b3b386374713a1cbf
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-10-26 12:19:57 +0900

    Tweak configuration to use Page Function

--- functions/api/index.ts
+++ functions/api/index.ts
@@ -50,11 +50,11 @@ interface ApiResponse {
   messages: any[];
 }
 
-export const onRequest = async ({ request, env }) => {
+export const onRequestPost = async ({ request, env }) => {
   const AI_GATEWAY_PROJECT = env.AI_GATEWAY_PROJECT;
   const API_KEY = env.CLOUDFLARE_API_KEY;
 
-  const { message } = await request.text();
+  const { message } = await request.body();
 
   const url = `https://gateway.ai.cloudflare.com/v1/${AI_GATEWAY_PROJECT}/ielts-writing2-practice/workers-ai/@cf/meta/llama-3.2-3b-instruct`;
 
